0ecf6bc617432e33a749ee5653c0e20416c1c4d6
jamieon <jpkinnunen@gmail.com>
2015-01-26 03:43:42 +0200

    funtions corrected

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,29 @@
 ## to set up a "List" to show the whole processes of making a matrix, and getting the inverse matrix:
 
 makeCacheMatrix <- function(x = matrix()) {
-     
+     m <- NULL
+     set <- function(y) {
+          x <<- y
+          m <<- NULL
+     }
+     get <- function() x
+     setsolve <- function(solve) m <<- solve
+     getsolve <- function() m
+     list(set = set, get = get,
+          setsolve = setsolve,
+          getsolve = getsolve)
 }
 
-
-##  to examine if the result (inverse matrix) of applied matrix had existed or not:
+##  to examine if the result (inverse matrix) of applied matrix had existed or not, and to return the inverse:
 
 cacheSolve <- function(x, ...) {
-     
-     ## Return a matrix that is the inverse of 'x'
+     m <- x$getsolve()
+     if(!is.null(m)) {
+          message("getting cached data")
+          return(m)
+     }
+     data <- x$get()
+     m <- solve(data, ...)
+     x$setsolve(m)
+     m
 }
